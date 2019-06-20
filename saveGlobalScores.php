9d2338713351fdb6409211fb088cc66c00994674
barlow-doug <49971984+barlow-doug@users.noreply.github.com>
2019-06-19 23:06:05 -0700

    Add files via upload

---
+++ saveGlobalScores.php
@@ -0,0 +1,10 @@
+﻿<?php
+if (isset($_GET['scores'])){
+	$jsonFile = fopen("highScores.json", "w") or die("Unable to open file!");
+	$scores = $_GET['scores'];
+	fwrite($jsonFile, $scores);
+	fclose($jsonFile);
+} else {
+	die;
+}
+?>
